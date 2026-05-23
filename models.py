6bbd31158a885d697447e761a3999c64c7c86299
KK057 <bky8422@gmail.com>
2026-05-23 23:57:42 +0530

    commented

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from sqlalchemy import DateTime, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import Base
-
+#Database models (for saving that data to a database like PostgreSQL or SQLite). 
 
 class User(Base):
     __tablename__ = "users"
